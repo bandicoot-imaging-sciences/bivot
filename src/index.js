f25a465b78aba5e6d0935d0042f01d11f6d967a6
yellowtailfan <36425185+yellowtailfan@users.noreply.github.com>
2020-09-07 11:05:44 +1000

    Import Bivot CSS into Bivot React for downstream.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 export { externalScripts as bivotScripts } from './BivotReact.js';
 export { useScripts } from './utils/hooksLib';
 export { default as Bivot } from './BivotReact.js';
+
+import './bivot-js/bivot.css';
